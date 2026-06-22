fb1dcea344342468a41c708bb2a14218e09a3e17
CoachFroden <frode@vibliz.com>
2026-06-22 21:09:02 +0200

    Add files via upload

--- players.js
+++ players.js
@@ -6,25 +6,25 @@ const players = [
   { name: "Thage", positions: ["Keeper"], level: 7 },
   { name: "Gabriel", positions: ["Forsvar"], level: 5 },
   { name: "Ask", positions: ["Forsvar"], level: 10 },
-  { name: "Brage", positions: ["Forsvar"], level: 5 },
+  { name: "Brage", positions: ["Forsvar"], level: 6 },
 
   { name: "Sverre", positions: ["Midtbane"], level: 6 },
   { name: "Snorre", positions: ["Midtbane", "Forsvar"], level: 7 },
-  { name: "Torvald", positions: ["Midtbane", "Forsvar"], level: 4 },
-  { name: "Lars", positions: ["Midtbane", "Forsvar"], level: 7 },
+  //{ name: "Torvald", positions: ["Midtbane", "Forsvar"], level: 4 },
+  { name: "Lars", positions: ["Midtbane", "Forsvar"], level: 8 },
   { name: "Samuel", positions: ["Midtbane", "Spiss"], level: 7 },
   { name: "Theo", positions: ["Midtbane", "Spiss"], level: 6 },
   //{ name: "William", positions: ["Midtbane"], level: 6 },
 
   { name: "Noah", positions: ["Midtbane", "Spiss"], level: 7 },
-  { name: "Nico", positions: ["Midtbane", "Spiss"], level: 12 },
+  { name: "Nico", positions: ["Midtbane", "Spiss"], level: 11 },
   { name: "Nytveit", positions: ["Spiss"], level: 1 },
   { name: "Lukas", positions: ["Midtbane"], level: 5 },
 
   { name: "Oliver", positions: ["Spiss"], level: 3 },
   { name: "Liam", positions: ["Spiss"], level: 10 },
-  { name: "Sondre", positions: ["Forsvar"], level: 5 },
-  { name: "Martin", positions: ["Forsvar"], level: 7 },
+  { name: "Sondre", positions: ["Forsvar"], level: 6 },
+  { name: "Martin", positions: ["Forsvar"], level: 8 },
   { name: "Timur", positions: ["Spiss", "Midtbane"], level: 2 },
   
   
